4fd662e7dbc02e8e05a32cac99f5f8da803d0bb1
Dónal Flanagan <Donal-Flanagan@users.noreply.github.com>
2022-01-20 12:00:11 +0100

    Delete api_keys.py

--- wk-6_data_pipelines/api_keys.py
+++
@@ -1,8 +0,0 @@
-OWM_key = "edf42bc06dd44ecc0a0d9b992051e693"
-rapid_api_key = "f1c0e4928cmsh83f8c0fc2ce783fp10d567jsn8b76323aa888"
-aws_mysql_host = "wbs-df-gans-project-db.c1vibcyrvq4q.eu-central-1.rds.amazonaws.com"
-aws_mysql_user = "admin"
-aws_mysql_password = "bloodyaws098!"
-
-local_mysql_user = "donal"
-local_mysql_password = "guest123"
