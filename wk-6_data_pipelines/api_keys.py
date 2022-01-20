b63f133eaf1562021cc99ca84cfb2b5714af3227
Donal Flanagan <donaloflanagain@gmail.com>
2022-01-20 11:59:26 +0100

    updated .gitignore

--- wk-6_data_pipelines/api_keys.py
+++ wk-6_data_pipelines/api_keys.py
@@ -3,3 +3,6 @@ rapid_api_key = "f1c0e4928cmsh83f8c0fc2ce783fp10d567jsn8b76323aa888"
 aws_mysql_host = "wbs-df-gans-project-db.c1vibcyrvq4q.eu-central-1.rds.amazonaws.com"
 aws_mysql_user = "admin"
 aws_mysql_password = "bloodyaws098!"
+
+local_mysql_user = "donal"
+local_mysql_password = "guest123"
